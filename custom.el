a790c166dbd435fc75fd7eb0e4436b26523ebd19
kowodo <stepan.krivanec@gmail.com>
2016-07-14 09:25:20 +0200

    Excluding non-important files from repository

---
+++ custom.el
@@ -0,0 +1,17 @@
+; some random string
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(canlock-password "7bac8234aeae3716dcd939e8a4424f1d085736be")
+ '(org-log-into-drawer t)
+ '(send-mail-function (quote smtpmail-send-it))
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 587))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,28 @@
+;;; Begin initialization
+
+;;; Set up package
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+;;; Bootstrap use-package
+;; Install use-package if it's not already installed.
+;; use-package is used to configure the rest of the packages.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;; what was written on use-package github page
+;; https://github.com/jwiegley/use-package
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)
+;(setq use-package-verbose t)
+
+
+;;; Load the config
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+
+
