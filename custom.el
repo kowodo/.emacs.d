5648476e50d728b93bd5191c0b742acab92a3a87
kowodo <stepan.krivanec@gmail.com>
2016-11-29 22:05:13 +0100

    added custom.el ... it should solve merging git repositories

--- custom.el
+++ custom.el
@@ -4,11 +4,88 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#ded6c5" "#f71010" "#028902" "#ef8300" "#0000ff" "#a020f0" "#358d8d" "#262626"])
  '(canlock-password "7bac8234aeae3716dcd939e8a4424f1d085736be")
+ '(custom-safe-themes
+   (quote
+    ("7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" "5e3fc08bcadce4c6785fc49be686a4a82a356db569f55d411258984e952f194a" "4e753673a37c71b07e3026be75dc6af3efbac5ce335f3707b7d6a110ecb636a3" default)))
+ '(diary-entry-marker (quote font-lock-variable-name-face))
+ '(emms-mode-line-icon-image-cache
+   (quote
+    (image :type xpm :ascent center :data "/* XPM */
+static char *note[] = {
+/* width height num_colors chars_per_pixel */
+\"    10   11        2            1\",
+/* colors */
+\". c #358d8d\",
+\"# c None s None\",
+/* pixels */
+\"###...####\",
+\"###.#...##\",
+\"###.###...\",
+\"###.#####.\",
+\"###.#####.\",
+\"#...#####.\",
+\"....#####.\",
+\"#..######.\",
+\"#######...\",
+\"######....\",
+\"#######..#\" };")))
+ '(fci-rule-color "#f6f0e1")
+ '(gnus-logo-colors (quote ("#259ea2" "#adadad")))
+ '(gnus-mode-line-image-cache
+   (quote
+    (image :type xpm :ascent center :data "/* XPM */
+static char *gnus-pointer[] = {
+/* width height num_colors chars_per_pixel */
+\"    18    13        2            1\",
+/* colors */
+\". c #358d8d\",
+\"# c None s None\",
+/* pixels */
+\"##################\",
+\"######..##..######\",
+\"#####........#####\",
+\"#.##.##..##...####\",
+\"#...####.###...##.\",
+\"#..###.######.....\",
+\"#####.########...#\",
+\"###########.######\",
+\"####.###.#..######\",
+\"######..###.######\",
+\"###....####.######\",
+\"###..######.######\",
+\"###########.######\" };")))
+ '(hl-sexp-background-color "#efebe9")
  '(org-log-into-drawer t)
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "smtp.gmail.com")
- '(smtpmail-smtp-service 587))
+ '(smtpmail-smtp-service 587)
+ '(vc-annotate-background "#f6f0e1")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#d81212")
+     (40 . "#f71010")
+     (60 . "#6a621b")
+     (80 . "#958323")
+     (100 . "#ef8300")
+     (120 . "#9ca30b")
+     (140 . "#008b45")
+     (160 . "#077707")
+     (180 . "#028902")
+     (200 . "#1c9e28")
+     (220 . "#3cb368")
+     (240 . "#0d7b72")
+     (260 . "#358d8d")
+     (280 . "#286060")
+     (300 . "#0505cc")
+     (320 . "#0000ff")
+     (340 . "#2c53ca")
+     (360 . "#a020f0"))))
+ '(vc-annotate-very-old-color "#a020f0"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
