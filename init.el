9e57fbbf33bd38788b2be6d6dedb740ee292c2a3
kowodo <stepan.krivanec@gmail.com>
2018-05-26 23:01:21 +0200

    removed diminish require, configuration of melpa

--- init.el
+++ init.el
@@ -1,9 +1,22 @@
 ;;; Begin initialization
 
 ;;; Set up package
+;;(require 'package)
+;;(add-to-list 'package-archives
+;;             '("melpa" . "http://melpa.org/packages/") t)
+;;(package-initialize)
+
+
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 
 ;;; Bootstrap use-package
@@ -17,7 +30,7 @@
 ;; https://github.com/jwiegley/use-package
 (eval-when-compile
   (require 'use-package))
-(require 'diminish)                ;; if you use :diminish
+;;(require 'diminish)                ;; if you use :diminish
 (require 'bind-key)
 ;(setq use-package-verbose t)
 
